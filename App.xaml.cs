0d1ab9069cef9bda01476c4a58231cc838781f74
MaelElseware <khelben1@gmail.com>
2025-05-29 13:41:34 +0200

    adding screenshot for readme.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * Move It Up! - Trivia & Exercise Application
+ * Copyright (C) 2025 Mael VIGNAUX
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
